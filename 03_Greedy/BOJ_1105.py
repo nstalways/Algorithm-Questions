c7a7ed6a426011c951d2d872667e34699b639034
nstalways <nstalways@gmail.com>
2023-10-06 19:30:19 +0900

    [Solve] BOJ1105_팔

---
+++ 03_Greedy/BOJ_1105.py
@@ -0,0 +1,36 @@
+import sys
+
+# l <= r
+def solution(l: str, r: str):
+    ans = 0
+    
+    # padding
+    min_len, max_len = len(l), len(r)
+    l = '0' * (max_len - min_len) + l
+
+    # 차이 계산
+    diff = int(r) - int(l)
+    len_diff = len(str(diff))
+
+    # l - r 길이가 2보다 작다
+    if len_diff < 2:
+        for idx in range(max_len):
+            if (l[idx], r[idx]) == ('8', '8'):
+                ans += 1
+        
+        print(ans)
+        
+    # 2 이상이면
+    else:
+        idx_to_check = [i for i in range(max_len - len_diff)]
+        for idx in idx_to_check:
+            if (l[idx], r[idx]) == ('8', '8'):
+                ans += 1
+        
+        print(ans)
+
+
+if __name__ == "__main__":
+    L, R = sys.stdin.readline().split()
+
+    solution(L, R)
