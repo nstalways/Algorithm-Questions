34d8546008e0255e04b0cfda55bdcf2e48c2129e
nstalways <nstalways@gmail.com>
2023-10-17 20:27:08 +0900

    [Optimize] BOJ14500_테트로미노

--- 02_Brute_Force/BOJ_14500.py
+++ 02_Brute_Force/BOJ_14500.py
@@ -1,5 +1,6 @@
 import sys
 from pprint import pprint
+import time
 
 def solution(n_rows, n_cols, paper):
     visited = [[False] * n_cols for _ in range(n_rows)]
@@ -64,6 +65,55 @@ def solution(n_rows, n_cols, paper):
     print(ans)
 
 
+def solution2(n_rows, n_cols, paper):
+    visited = [[False] * n_cols for _ in range(n_rows)]
+    max_val = max(map(max, paper))
+
+    ans = 0
+
+    dr = [-1, 1, 0, 0]
+    dc = [0, 0, -1, 1]
+
+    def dfs(r, c, cum_sum, cnt):
+        nonlocal ans
+
+        # BOJ 기준 이 조건 하나만 없어도 solution2는 시간초과가 발생함
+        if cum_sum + max_val * (4 - cnt) <= ans:
+            return
+
+        if cnt == 4:
+            ans = max(ans, cum_sum)
+            return
+        
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+
+            if nr < 0 or nr >= n_rows or \
+                nc < 0 or nc >= n_cols:
+                continue
+
+            if visited[nr][nc]:
+                continue
+
+            if cnt == 2:
+                visited[nr][nc] = True
+                dfs(r, c, cum_sum + paper[nr][nc], cnt + 1)
+                visited[nr][nc] = False
+
+            visited[nr][nc] = True
+            dfs(nr, nc, cum_sum + paper[nr][nc], cnt + 1)
+            visited[nr][nc] = False
+
+    for r in range(n_rows):
+        for c in range(n_cols):
+            visited[r][c] = True
+            dfs(r, c, paper[r][c], cnt=1)
+            visited[r][c] = False
+
+    print(ans)
+
+
 if __name__ == "__main__":
     input = sys.stdin.readline
 
@@ -72,4 +122,11 @@ if __name__ == "__main__":
     for _ in range(N):
         paper.append(list(map(int, input().split())))
 
+    start = time.time()
     solution(N, M, paper)
+    print(f'Execution time of solution 1: {time.time() - start:.4f}')
+    print('-' * 20)
+
+    start = time.time()
+    solution2(N, M, paper)
+    print(f'Execution time of solution 2: {time.time() - start:.4f}')
