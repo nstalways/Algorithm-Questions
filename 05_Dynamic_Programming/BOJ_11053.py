3ece8fa8dbc4ad504b99813ca19aa43d70c67686
nstalways <nstalways@gmail.com>
2023-10-21 15:46:03 +0900

    update: docstring

--- 05_Dynamic_Programming/BOJ_11053.py
+++ 05_Dynamic_Programming/BOJ_11053.py
@@ -1,6 +1,13 @@
 import sys
 
 def solution(n, data):
+    """
+    dp 배열: 자기 자신을 포함한 현재까지 가장 긴 증가하는 부분 수열의 길이
+
+    1. i번째 숫자와 i - 1번째 숫자까지 대/소 비교를 수행
+    2. 더 큰 값이고, dp 배열에서 부분 수열의 길이 또한 더 길다면 값을 할당.
+    3. 자기 자신을 추가
+    """
     dp = [0] * n
     for i in range(n):
         for j in range(i):
