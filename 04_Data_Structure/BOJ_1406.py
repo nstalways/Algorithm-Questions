301be7bbc67b3ea6e188a7a76f66d509b69b5e4f
nstalways <nstalways@gmail.com>
2023-10-25 19:20:45 +0900

    [Optimize] BOJ1406_에디터

--- 04_Data_Structure/BOJ_1406.py
+++ 04_Data_Structure/BOJ_1406.py
@@ -21,6 +21,35 @@ def solution(init_s, M, cmds):
 
     res = ''.join(list(q1) + list(q2))
     print(res)
+
+
+def solution2():
+    """입력을 반복문 내에서 받아 시간 최적화"""
+    input = sys.stdin.readline
+
+    init_s = list(input().strip())
+    M = int(input().strip())
+
+    q1 = deque(init_s)
+    q2 = deque([])
+
+    for _ in range(M):
+        cmd = input().split()
+        
+        if cmd[0] == 'L':
+            if q1:
+                q2.appendleft(q1.pop())
+        elif cmd[0] == 'D':
+            if q2:
+                q1.append(q2.popleft())
+        elif cmd[0] == 'B':
+            if q1:
+                q1.pop()
+        else:
+            q1.append(cmd[1])
+            
+    res = ''.join(list(q1) + list(q2))
+    print(res)
     
 
 if __name__ == "__main__":
