b156108c754817891723f49be5e35b557ef348b6
nstalways <nstalways@gmail.com>
2023-11-03 13:52:37 +0900

    [Study] BOJ1018_체스판_다시_칠하기

--- 06_Graph/BOJ_1018.py
+++ 06_Graph/BOJ_1018.py
@@ -1,64 +1,68 @@
 import sys
-from typing import List
-from pprint import pprint
 from collections import deque
-from copy import deepcopy
+input = sys.stdin.readline
 
-def solution(board: List[list], start: tuple = (0, 0), limit: tuple = (8, 8)):
-    visited = [start]
-    queue = deque([start])
+# 입력
+N, M = map(int, input().split())
+board = [list(input().strip()) for _ in range(N)]
 
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+# 풀이
+"""
+왼쪽 위 상태를 기준으로 8x8을 탐색하고, 결과를 갱신한다.
+왼쪽 위 체스의 원래 색깔과 반전한 색깔의 경우 또한 고려해야 한다.
+"""
+def bfs(r, c):
+    dr = [-1, 1, 0, 0]
+    dc = [0, 0, -1, 1]
 
-    answer = 0
-    while queue:
-        x, y = queue.popleft()
+    res = [float('inf')]
 
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+    for color in ['B', 'W']:
+        visited = [[False] * M for _ in range(N)]
 
-            if nx < 0 or nx >= limit[0] or ny < 0 or ny >= limit[1]:
-                continue
-            
-            if (nx, ny) not in visited:
-                queue.append((nx, ny))
-                visited.append((nx, ny))
+        queue = deque([(r, c, color)]) # 좌표 + 색깔
+        visited[r][c] = True
+
+        cnt = 0
+        if color != board[r][c]:
+            cnt += 1
+        
+        append_flag = True
+        while queue:
+            if cnt >= res[-1]:
+                append_flag = False
+                break
+
+            cur_r, cur_c, cur_color = queue.popleft()
+
+            for i in range(4):
+                nr = cur_r + dr[i]
+                nc = cur_c + dc[i]
+
+                if nr < r or nr >= r + 8 or \
+                    nc < c or nc >= c + 8:
+                    continue
+
+                if visited[nr][nc]:
+                    continue
                 
-                if board[x][y] == board[nx][ny]:
-                    if board[x][y] == "B":
-                        board[nx][ny] = "W"
-                    else:
-                        board[nx][ny] = "B"
-                    
-                    answer += 1
+                next_color = board[nr][nc]
+                if cur_color == next_color:
+                    next_color = 'B' if cur_color == 'W' else 'W'
+                    cnt += 1
+                
+                queue.append((nr, nc, next_color))
+                visited[nr][nc] = True
+        
+        if append_flag:
+            res.append(cnt)
 
-    return answer
+    return min(res)
 
+ans = float("inf")
+for r in range(N - 8 + 1):
+    for c in range(M - 8 + 1):
+        res = bfs(r, c)
+        ans = min(ans, res)
 
-if __name__ == "__main__":
-    # 입력
-    N, M = map(int, input().split())
-    chess_board = []
-
-    for _ in range(N):
-        chess_board.append(list(sys.stdin.readline().strip()))
-    
-    # 8 x 8 크기로 자르기
-    answer = float('inf')
-    row_limit, col_limit = 8, 8
-    
-    for col_start in range(M - col_limit + 1):
-        for row_start in range(N - row_limit + 1):
-            chess_board_8x8 = []
-
-            for i in range(row_limit):
-                chess_board_8x8.append(chess_board[row_start + i][col_start:col_start + col_limit])
-            
-            for start in [(0, 0), (0, 7), (7, 0), (7, 7)]:
-                chess_board_8x8_cp = deepcopy(chess_board_8x8)
-                answer = min(answer, solution(chess_board_8x8_cp, start))
-    
-    print(answer)
-    
+print(ans)
