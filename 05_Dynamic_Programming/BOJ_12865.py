cbf1dcb7c38ac99cd0560a0dd2d468372fa284e0
nstalways <nstalways@gmail.com>
2023-11-08 18:42:06 +0900

    [Solve2] BOJ12865_평범한_배낭

--- 05_Dynamic_Programming/BOJ_12865.py
+++ 05_Dynamic_Programming/BOJ_12865.py
@@ -6,20 +6,38 @@ N, K = map(int, input().split())
 wandv = [0] + [list(map(int, input().split())) for _ in range(N)]
 
 # 풀이
-dp_bags = [[False] * (N + 1) for _ in range(K + 1)]
-dp_values = [0] * (K + 1)
+def solution1():
+    dp_bags = [[False] * (N + 1) for _ in range(K + 1)]
+    dp_values = [0] * (K + 1)
 
-for i in range(1, K + 1):
-    for j in range(1, N + 1):
-        w, v = wandv[j]
-        if w > i:
-            continue
+    for i in range(1, K + 1):
+        for j in range(1, N + 1):
+            w, v = wandv[j]
+            if w > i:
+                continue
 
-        tmp = dp_values[i - w] + v
-        if dp_values[i] < tmp and not dp_bags[i - w][j]:
-            dp_values[i] = tmp
-            
-            dp_bags[i] = dp_bags[i - w][:]
-            dp_bags[i][j] = True
+            tmp = dp_values[i - w] + v
+            if dp_values[i] < tmp and not dp_bags[i - w][j]:
+                dp_values[i] = tmp
+                
+                dp_bags[i] = dp_bags[i - w][:]
+                dp_bags[i][j] = True
 
-print(max(dp_values))
+    print(max(dp_values))
+
+def knapsack():
+    dp = [[0] * (K + 1) for _ in range(N + 1)]
+
+    for i in range(1, N + 1):
+        for j in range(1, K + 1):
+            w, v = wandv[i]
+            if w > j:
+                dp[i][j]= dp[i - 1][j]
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v)
+
+    print(max(dp[-1]))
+
+
+solution1()
+knapsack()
