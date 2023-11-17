cf2909f2683d3d882dc49891bdda79692c29e1dc
nstalways <nstalways@gmail.com>
2023-11-17 13:35:05 +0900

    [Optimize] BOJ11582_치킨_TOP_N

--- 00_Etc/BOJ_11582.py
+++ 00_Etc/BOJ_11582.py
@@ -9,20 +9,10 @@ k = int(input().strip())
 # 풀이
 """
 구현
+순서대로 정렬할 필요없이, k명의 회원이 정렬할 때 결과를 바로 출력하면 됨.
+이전 단계의 정렬 상태와 관계없이, k명의 회원이 정렬할 때 범위 내 모든 치킨집을 확인해야하기 때문
+k = n // x -> x = n // k-> x: k명의 회원이 정렬할 치킨집의 수
 """
-ans = infos[:]
-
-factor = 2
-while True:
-    tmp = []
-    for i in range(0, N, factor):
-        tmp.extend(sorted(ans[i:i + factor]))
-
-    ans = tmp
-
-    if (N // factor) == k:
-        print(*ans)
-        break
-
-    factor *= 2
-    
+factor = N // k
+for i in range(0, N, factor):
+    print(*sorted(infos[i:i + factor]), end= ' ')
